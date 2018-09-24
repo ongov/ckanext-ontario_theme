45b9ba80dda2e7c349342c887f26804fa0aa1495
Cody Boyko <cody.boyko2@ontario.ca>
2018-09-24 15:32:36 -0400

    Task: Update bs2 related files.
    Make minimal updates to bootstrap 2 theme to
    enable CKAN 2.8 support.

--- ckanext/ontario_theme/plugin.py
+++ ckanext/ontario_theme/plugin.py
@@ -11,3 +11,7 @@ class OntarioThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ontario_theme')
+        # Uncomment these to use bootstrap 2 theme and comment out
+        # the above template and resource directories.
+        # toolkit.add_template_directory(config_, 'templates-bs2')
+        # toolkit.add_resource('fanstatic-bs2', 'ontario_theme')
