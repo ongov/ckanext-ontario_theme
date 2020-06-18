d158eb22dc63a57590ddd6411356a9b8da83ec8e
Cody Boyko <codywboyko@gmail.com>
2020-06-18 16:56:48 -0400

    Improvment: Keep same public directory.

--- ckanext/ontario_theme/plugin.py
+++ ckanext/ontario_theme/plugin.py
@@ -280,7 +280,7 @@ class OntarioThemeExternalPlugin(plugins.SingletonPlugin, DefaultTranslation):
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates/external')
-        toolkit.add_public_directory(config_, 'public/external')
+        toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic/external', 'ontario_theme_external')
 
         config_['scheming.dataset_schemas'] = """
