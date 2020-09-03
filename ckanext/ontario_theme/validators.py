b6bc54c75f8c7dad5bf8e17d73b3af977822f9b8
Bianca Sayan <bianca.sayan@ontario.ca>
2020-09-03 08:46:41 -0400

    Task: Add specific tag validator messages and strip.
    Add specific tag validator messages for commas and consecutive spaces. Also, strip the value.

--- ckanext/ontario_theme/validators.py
+++ ckanext/ontario_theme/validators.py
@@ -3,10 +3,18 @@
 def tag_name_validator(value, context):
     import re
     from ckan.common import _
+
+    value = value.strip()
+
+    if u',' in value:
+        raise Invalid(_(u'Tag "%s" may not contain commas') % (value,))
+    if u'  ' in value:
+        raise Invalid(
+            _(u'Tag "%s" may not contain consecutive spaces') % (value,))
+
     tagname_match = re.compile(ur'[\w \’\'\-.]*$', re.UNICODE)
+
     if not tagname_match.match(value):
         raise Invalid(_('Tag "%s" must be alphanumeric '
                         'characters or symbols: \'’-_.') % (value))
     return value
-
-
