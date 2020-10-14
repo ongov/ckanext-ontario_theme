303569a1f81a39e8760f917852324969b2fbbb03
Bianca Sayan <bianca.sayan@ontario.ca>
2020-10-14 14:52:03 -0400

    Task: Refactor required_if.
    Required_if validator refactored to be neater.

--- ckanext/ontario_theme/validators.py
+++ ckanext/ontario_theme/validators.py
@@ -6,15 +6,20 @@ import logging
 import json
 log = logging.getLogger(__name__)
 
-def extract_fluent(value):
+def has_missing_value(value):
 	try:
 		check_value = json.loads(value)
 		if isinstance(check_value, dict):
-			return check_value
+			for fkey, fvalue in check_value.items():
+				if not fvalue:
+					return True
 		else:
-			return value		
+			if not value:
+				return True	
 	except:
-		return value
+		if not value:
+			return True
+	return False
 
 
 def required_if(dependent_field, dependent_value):
@@ -22,20 +27,11 @@ def required_if(dependent_field, dependent_value):
 
     def callable(key, data, errors, context):
     	value = data.get(key)
-    	check_value = extract_fluent(value)
+    	
     	if data.get((dependent_field,)) == dependent_value:
-    		# is this a fluent field?
-			if isinstance(check_value, dict):
-				for fkey, fvalue in check_value.items():
-					if not fvalue:
-						raise Invalid(
-		                	_(u'This is required'))    			
-			else:
-				if not check_value:
-					raise Invalid(
-		                _(u'This is required'))
-        return
-
+    		if has_missing_value(value):
+    			raise Invalid(_(u'This is required'))
+    			#errors[key].append(_(u'This is required'))  			
     return callable
 
 def conditional_save(dependent_field, dependent_value):
