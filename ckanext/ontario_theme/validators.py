37200046468144b2b03e30f59f2cb131d2ee7d56
Bianca Sayan <bianca.sayan@ontario.ca>
2020-10-16 09:27:45 -0400

    Task: Use validator error list to throw error.
    Using 'raise invalid' with fluent fields does not attach an error to the specific field. Using the error dict does attach an error to the specific field.

--- ckanext/ontario_theme/validators.py
+++ ckanext/ontario_theme/validators.py
@@ -6,32 +6,25 @@ import logging
 import json
 log = logging.getLogger(__name__)
 
-def has_missing_value(value):
-	try:
-		check_value = json.loads(value)
-		if isinstance(check_value, dict):
-			for fkey, fvalue in check_value.items():
-				if not fvalue:
-					return True
-		else:
-			if not value:
-				return True	
-	except:
-		if not value:
-			return True
-	return False
-
 
 def required_if(dependent_field, dependent_value):
     ''' require this field only if the dependent field has a certain value'''
 
     def callable(key, data, errors, context):
     	value = data.get(key)
-    	
     	if data.get((dependent_field,)) == dependent_value:
-    		if has_missing_value(value):
-    			raise Invalid(_(u'This is required'))
-    			#errors[key].append(_(u'This is required'))  			
+			try:
+				check_value = json.loads(value)
+				if isinstance(check_value, dict):
+					for fkey, fvalue in check_value.items():
+						if not fvalue:
+							errors[key[:-1] + (key[-1] + '-' + fkey,)] = [_(u'This is required')]
+				else:
+					if not value:
+						errors[key] = [_(u'This is required')]	
+			except:
+				if not value:
+					errors[key] = [_(u'This is required')]	 			
     return callable
 
 def conditional_save(dependent_field, dependent_value):
