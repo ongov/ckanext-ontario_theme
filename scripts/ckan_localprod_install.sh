69286ccf3fde6e8c5790f36f8ab28de51066559b
= <=>
2022-07-27 15:14:53 -0400

    fix typo in ckan_localprod script and add CKAN_INI to supervisor config file

--- scripts/ckan_localprod_install.sh
+++ scripts/ckan_localprod_install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
+source ./helper_functions.sh
 
-export CKANINIPATH="/etc/ckan/default/ckan.ini"
-export SITEURL='ckan.site_url = http'
-export SITEURL_REPLACEMENT='ckan.site_url = https'
-export SSLNAME='ckan_host'
+export CKANINIROOT="/etc/ckan/default/"
+export CKANINIPATH=$CKANINIROOT"ckan.ini"
 
 # uwsgi script & server
 echo $SUDOPASS | sudo -S -k cp /usr/lib/ckan/default/src/ckan/wsgi.py /etc/ckan/default/
+echo $SUDOPASS | sudo -S -k chown www-data /etc/ckan/default/wsgi.py
 . /usr/lib/ckan/default/bin/activate
-pip3 install uwsgi
+pip3 install -Iv uwsgi==2.0.20
 echo $SUDOPASS | sudo -S -k cp /usr/lib/ckan/default/src/ckan/ckan-uwsgi.ini /etc/ckan/default/
 
 # configure supervisor
@@ -16,26 +16,27 @@ echo $SUDOPASS | sudo -S -k apt-get -y install supervisor=4.1.0-1ubuntu1
 echo $SUDOPASS | sudo -S -k cp `pwd`/../config/supervisor/ckan-uwsgi.conf /etc/supervisor/conf.d/ckan-uwsgi.conf
 
 # install an email server 
+# TODO
 
 # install and configure nginx
 echo $SUDOPASS | sudo -S -k apt-get -y install nginx=1.18.0-0ubuntu1.3
-cp `pwd`/../config/nginx/localckan /etc/nginx/sites-available/
+echo $SUDOPASS | sudo -S -k cp `pwd`/../config/nginx/local_ckan_ssl /etc/nginx/sites-available/
+echo $SUDOPASS | sudo -S -k ln -s /etc/nginx/sites-available/local_ckan_ssl /etc/nginx/sites-enabled/
+
 # Generate and configure a certificate
 # Create SSL certificate
 # https://docs.ckan.org/en/2.9/maintaining/configuration.html#ckan-devserver-ssl-cert
+SSLNAME=$CKANINIROOT'ckan_host'
 openssl genrsa 2048 > $SSLNAME.key
 chmod 400 $SSLNAME.key
 openssl req -new -x509 -nodes -sha256 -days 3650 -key $SSLNAME.key -subj "/C=CA/ST=ON/L=./O=ODS/OU=./CN=." > $SSLNAME.cert
 
-# Modify ckan.ini to point ckan.site_url to localhost instead of localhost:5000
-# Backup original config file
-cd /etc/nginx/sites-available/
-cp ckan ckan_orig
-mv localckan ckan
-
 # Modify site_url in ckan.ini
-sed -i -r 's/'"$SITEURL"'/'"$SITEURL_REPLACEMENT"'/g' $CKANINIPATH
+
+SITEURL="ckan.site_url = http://localhost:5000"
+SITEURL_REPLACEMENT="ckan.site_url = https://localhost"
+replace_str_in_ckan_ini "$SITEURL" "$SITEURL_REPLACEMENT"
 
 # Restart NGINX and supervisor
 echo $SUDOPASS | sudo -S -k service nginx restart
-echo $SUDOPASS | sudo -S -k supervisor restart
+echo $SUDOPASS | sudo -S -k service supervisor restart
