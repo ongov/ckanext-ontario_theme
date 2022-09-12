7f9f15016c52a44fb7290affe6c0d6e6840d145d
KatiRG <cnangini@gmail.com>
2022-09-12 09:31:45 -0400

    WIP #230: replace mimetype checks with magic and refactor

--- ckanext/ontario_theme/resource_upload.py
+++ ckanext/ontario_theme/resource_upload.py
@@ -34,53 +34,51 @@ _max_image_size = None
 
 
 def accepted_resource_formats():
-    '''Returns list of accepted file extensions.
+    '''Returns list of accepted file extensions and a
+    list of accepted mime types.
     '''
-    resource_formats = []
     resource_format_path = os.path.join(os.path.dirname(__file__),
                                         'accepted_resource_formats.json')
+    resource_exts = []
+    resource_types = []
     with open(resource_format_path) as format_file:
         file_resource_formats = json.loads(format_file.read())
 
         for format_line in file_resource_formats:
-            resource_formats.append(format_line[0].upper())
-    return resource_formats
+            resource_exts.append(format_line[0].upper())
+            resource_types.append(format_line[2])
+    return resource_exts, resource_types
 
-
-def allowed_file(filename):
+def allowed_ext(filename):
     '''Returns boolean. Checks if the file extension is acceptable.
     '''
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].upper() in accepted_resource_formats()
+    resource_exts, resource_types = accepted_resource_formats()
+    return filename.rsplit('.', 1)[1].upper() in resource_exts
 
-def scanfile(this_deepcopy):
-        """Opens the deep copy of the SpooledTemporaryFile containing
-        the uploaded file.
+def allowed_mimetype(magic_mimetype):
+    '''Returns boolean. Checks if the magic mimetype is acceptable.
+    '''
+    resource_exts, resource_types = accepted_resource_formats()
+    # HACK UNTIL UNIX FILE UPGRADED
+    if magic_mimetype == 'application/csv':
+        return True
 
-        Writes the binary contents of the deep copy into a temporary file.
+    if magic_mimetype == 'text/xml':
+        return True
 
-        Inputs the temporary file (by name) into python-magic for scanning.
+    return magic_mimetype in resource_types
 
-        Args:
-            this_deepcopy (SpooledTemporaryFile): object containing uploaded file
-
-        Returns:
-            Contents of python-magic.
-        """
-        # Declare object to open temporary file for writing
-        temp = tempfile.NamedTemporaryFile()
-
-        try:
-            with this_deepcopy as out_f:
-                out_f.seek(0)
-                data = out_f.read()
-                temp.write(data)
-                temp.seek(0)
-                contents = magic.from_file(temp.name)
-        finally:
-            temp.close()
-        
-        return contents
+def alert_invalidfile(resource, this_filename):
+    log.error('Upload: Invalid upload file format.{}'.format
+        (this_filename))
+    # remove file - by default a resource can be added without any
+    # values
+    resource['url'] = None
+    resource['url_type'] = ''
+    raise logic.ValidationError(
+        {'upload':
+        ['Invalid upload file format, file has been removed.']}
+    )
 
 class ResourceUpload(DefaultResourceUpload):
     def __init__(self, resource):
@@ -127,57 +125,24 @@ class ResourceUpload(DefaultResourceUpload):
             self.filesize = self.upload_file.tell()
             # go back to the beginning of the file buffer
             self.upload_file.seek(0, os.SEEK_SET)
-            uploaded_file_deepcopy = copy.deepcopy(self.upload_file)
-
-            # MODIFICATION START: scan file with python-magic
-            # The with block beclow will forever close self.upload_file
-            # and it will no longer be subsequently useable. Use a deep
-            # copy instead. 
-            # try:
-            #     with self.upload_file as out_f:
-            #         out_f.seek(0) 
-            #         data = out_f.read()
-            #         temp.write(data)
-            #         temp.seek(0)
-            #         contents = magic.from_file(temp.name)
-            #         print('magic: ', contents)
-            # finally:
-            #     temp.close()
-
-            # Use python-magic to check the contents of the deep copy
-            contents = scanfile(uploaded_file_deepcopy)
-            print('CONTENTS: ', contents)
-            # MODIFICATION END
 
             # MODIFICATION START: check extension
-            # Note: If resubmitting a failed form without clearing the file
-            # the ResourceUpload.upload function would be called skipping the
-            # init call.
-            if not allowed_file(self.filename):
-                log.error('Upload: Invalid upload file format.{}'.format
-                    (self.filename))
-                # remove file - by default a resource can be added without any
-                # values
-                resource['url'] = None
-                resource['url_type'] = ''
-                raise logic.ValidationError(
-                    {'upload':
-                    ['Invalid upload file format, file has been removed.']}
-                )
+            if  '.' in self.filename and not allowed_ext(self.filename):
+                alert_invalidfile(resource, self.filename)
             # MODIFICATION END
 
-            # check if the mimetype failed from guessing with the url
-            if not self.mimetype and config_mimetype_guess == 'file_ext':
-                self.mimetype = mimetypes.guess_type(self.filename)[0]
+            # MODIFICATION START: identify file type with python-magic
+            try:
+                self.mimetype = magic.from_buffer(self.upload_file.read(),
+                                                mime=True)
+                self.upload_file.seek(0, os.SEEK_SET)
 
-            if not self.mimetype and config_mimetype_guess == 'file_contents':
-                try:
-                    self.mimetype = magic.from_buffer(self.upload_file.read(),
-                                                    mime=True)
-                    self.upload_file.seek(0, os.SEEK_SET)
-                except IOError as e:
-                    # Not that important if call above fails
-                    self.mimetype = None
+                if not allowed_mimetype(self.mimetype):
+                    alert_invalidfile(resource, self.filename)
+            except IOError as e:
+                # Not that important if call above fails
+                self.mimetype = None
+            # MODIFICATION END
                 
         elif self.clear:
             resource['url_type'] = ''
