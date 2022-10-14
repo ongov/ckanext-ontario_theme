96d562f0713eeea98bf71486b95e3bacd2766c54
Admin <admin@admin.admin>
2022-10-14 20:21:41 +0000

    WIP gadownloads: add tracking for pageview download btn

--- ckanext/ontario_theme/fanstatic/internal/ontario_theme_download_tracker.js
+++ ckanext/ontario_theme/fanstatic/internal/ontario_theme_download_tracker.js
@@ -7,14 +7,7 @@ function trackDownload(resourceUrl, orgName, pkgTitle, groupName) {
 
   let fileName = resourceUrl.split('download/')[1];
 
-  if (groupName.length > 2) { 
-    // clean = groupName.replaceAll('&#39;','"').replaceAll('u"','"');
-    // groupName = JSON.parse("[" + clean + "]")[0][0]['name'];
-    groupName = "TBD";
-  } else {
-    groupName = "";
-  }
-  console.log('groupName after fix: ', groupName)
+  console.log('groupName: ', groupName)
 
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
