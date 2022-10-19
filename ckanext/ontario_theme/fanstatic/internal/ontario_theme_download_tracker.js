923943cffca692be06745e7947793a07f414d1ae
katiRG <cnangini@gmail.com>
2022-10-19 09:27:46 -0400

    WIP #237 (ckan2.9): prevent event layers from merging in GTM

--- ckanext/ontario_theme/fanstatic/internal/ontario_theme_download_tracker.js
+++ ckanext/ontario_theme/fanstatic/internal/ontario_theme_download_tracker.js
@@ -10,12 +10,16 @@ function trackDownload(resourceUrl, orgName, pkgTitle, groupName) {
   urlArray = resourceUrl.split('/');
   fileName = urlArray[urlArray.length - 1];
 
+  // Set variables from the Pageview event to undefined
+  // to prevent them from being collected in GTM
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
     'event': 'File Download',
     'ministryTag': orgName,
     'group': groupName,
     'datasetName': pkgTitle,
-    'dataResourceName': fileName
+    'dataResourceName': fileName,
+    'dataAvailability': undefined,
+    'resourceName': undefined
   });
 }
