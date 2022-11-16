7a7974369367968a07e2dc670e3767fa6b998065
aimal.khan@ontario.ca <aimal.khan@ontario.ca>
2022-11-16 09:39:00 -0500

    update nginx/supervisor installed versions

--- scripts/ckan_localprod_install.sh
+++ scripts/ckan_localprod_install.sh
@@ -16,11 +16,11 @@ pip3 install -Iv uwsgi==2.0.20
 echo $SUDOPASS | sudo -S -k cp /usr/lib/ckan/default/src/ckan/ckan-uwsgi.ini /etc/ckan/default/
 
 # configure supervisor
-echo $SUDOPASS | sudo -S -k apt-get -y install supervisor=4.1.0-1ubuntu1
+echo $SUDOPASS | sudo -S -k apt-get -y install supervisor=4.1.0*
 echo $SUDOPASS | sudo -S -k cp `pwd`/../config/supervisor/ckan-uwsgi.conf /etc/supervisor/conf.d/ckan-uwsgi.conf
 
 # install and configure nginx
-echo $SUDOPASS | sudo -S -k apt-get -y install nginx=1.18.0-0ubuntu1.3
+echo $SUDOPASS | sudo -S -k apt-get -y install nginx=1.18.0*
 echo $SUDOPASS | sudo -S -k cp `pwd`/../config/nginx/local_ckan_ssl /etc/nginx/sites-available/
 echo $SUDOPASS | sudo -S -k ln -s /etc/nginx/sites-available/local_ckan_ssl /etc/nginx/sites-enabled/
 
