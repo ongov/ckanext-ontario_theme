58d7b3cc31ed45ce92dffbeb60ff25ee1152c029
katiRG <cnangini@gmail.com>
2023-03-07 13:22:42 -0500

    WIP: add check for title_translated in sort method

--- ckanext/ontario_theme/plugin.py
+++ ckanext/ontario_theme/plugin.py
@@ -394,15 +394,16 @@ def get_keyword_count(keyword_lang, language):
 
     return keyword_count_by_lang
 
-def sort_page_items(obj, fields_dict):
-    '''Helper to return the page items object sorted by title_translated
-    for the current language.
+def sort_by_title_translated(obj, **kwargs):
+    '''Helper to return the page items object sorted by "title_translated"
+    dict. If this dict does not exist, "title" is used to sort since
+    "title" always exists.
     '''
-    field = fields_dict['field']
-    lang = fields_dict['lang']
-    reverse = fields_dict['reverse']
+    field = 'title_translated'
+    lang = kwargs['lang']
+    reverse = kwargs['reverse']
     return sorted(obj, 
-                  key=lambda x: x[field][lang], 
+                  key=lambda x: x[field][lang] if (field in x and lang in x[field]) else x['title'], 
                   reverse=reverse)
 
 
@@ -733,7 +734,7 @@ type data_last_updated
                 'ontario_theme_home_block_link': home_block_link,
                 'ontario_theme_get_group_datasets': get_group_datasets,
                 'ontario_theme_get_keyword_count': get_keyword_count,
-                'ontario_theme_sort_page_items': sort_page_items
+                'ontario_theme_sort_by_title_translated': sort_by_title_translated
                 }
 
     # IBlueprint
