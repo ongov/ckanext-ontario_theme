fe854ad02d49ffecd4184cbe45770c2273ee3a7f
Alex Green <aleexgreen16@gmail.com>
2023-07-14 16:44:34 -0400

    Fixes internal 500 error when using load_organization_data.py

--- ckanext/ontario_theme/resource_upload.py
+++ ckanext/ontario_theme/resource_upload.py
@@ -168,5 +168,6 @@ class ResourceUpload(DefaultResourceUpload):
         if url and not (resource.get('url_type') == 'upload') and not resource.get('format'):
             resource['format'] = 'WEB'
 
-        if not (resource.get('format') == 'GeoJSON'):
-            resource['format'] = resource['format'].upper()
+        if resource.get('format') and not (resource.get('format') == 'GeoJSON'):
+            resource_format = resource.get('format')
+            resource['format'] = resource_format.upper()
