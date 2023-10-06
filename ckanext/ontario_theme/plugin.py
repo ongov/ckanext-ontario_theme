76487571220b40a953321f40c9d17f72cc307cb8
Alex Green <alex.green@ontario.ca>
2023-10-06 14:43:57 -0400

    ckanext-validation stash popped

--- ckanext/ontario_theme/plugin.py
+++ ckanext/ontario_theme/plugin.py
@@ -895,11 +895,13 @@ class OntarioThemeExternalPlugin(plugins.SingletonPlugin, DefaultTranslation):
         toolkit.add_resource('fanstatic/external', 'ontario_theme_external')
 
         config_['scheming.dataset_schemas'] = """
+ckanext.validation.examples:ckan_default_schema.json
 ckanext.ontario_theme:schemas/external/ontario_theme_dataset.json
 """
         config_['scheming.presets'] = """
 ckanext.scheming:presets.json
 ckanext.fluent:presets.json
+ckanext.validation:presets.json
 """
         config_['scheming.organization_schemas'] = """
 ckanext.ontario_theme:schemas/ontario_theme_organization.json
@@ -932,11 +934,13 @@ class OntarioThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
 
         if 'scheming.dataset_schemas' not in config_:
             config_['scheming.dataset_schemas'] = """
+ckanext.validation.examples:ckan_default_schema.json
 ckanext.ontario_theme:schemas/internal/ontario_theme_dataset.json
 """
         config_['scheming.presets'] = """
 ckanext.scheming:presets.json
 ckanext.fluent:presets.json
+ckanext.validation:presets.json
 """
 
         config_['scheming.organization_schemas'] = """
