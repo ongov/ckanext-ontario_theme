c38019a16fedf7a24c5f234b3ab95f3f36d1683e
Alex Green <alex.green@ontario.ca>
2023-12-14 16:32:18 -0500

    Fixes bug when other filtera are selected

--- ckanext/ontario_theme/plugin.py
+++ ckanext/ontario_theme/plugin.py
@@ -1173,28 +1173,27 @@ type data_last_updated
         Uses fq_list instead of fq to allow for multiple facet fields to be
         selected and for the tagging to work.
         '''
+        access_level = 'access_level'
         fq = search_params.get('fq')
         facet_field = search_params.get('facet.field')
         exclude = "{!ex=al}access_level"
         tag = "{!tag=al}access_level"
         fq_list = []
-        if fq:
+        if facet_field:
+            facet_field[4] = exclude
+        else:
+            search_params.update({"facet.field": exclude})
+        if access_level not in request.params:
+            fq_list.append(tag + ":open")
+        if fq and access_level in request.params:
             facet_al = re.findall('access_level:"\\w+"', fq)
             fq_list = re.findall("(?:\".*?\"|\\S)+", fq)
             if facet_al and facet_field:
-                facet_field[4] = exclude
                 facet_tag = re.sub("access_level", tag, facet_al[0])
                 for i in range(len(fq_list)):
                     if fq_list[i] == facet_al[0]:
                         fq_list[i] = facet_tag
-                search_params.pop('fq')
-        else:
-            if 'access_level' not in request.params:
-                fq_list.append(tag + ":open")
-            if not facet_field:
-                search_params.update({"facet.field": exclude})
-            else:
-                facet_field.append(exclude)
+            search_params.pop('fq')
         search_params.update({"fq_list": fq_list,
                               "facet.field": facet_field})
         return num_resources_filter_scrub(search_params)
