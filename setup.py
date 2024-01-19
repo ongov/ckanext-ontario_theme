519caf3ee5ab7b4a940c87c2617fcf9ea16ced56
Aimal Khan <aimal.khan@ontario.ca>
2024-01-19 16:26:47 -0500

    release v2.1.13 (#487)
    upgrade version to v2.1.13 in setup.py.
    Release v2.1.13 includes the following features:
    
    * Fix datetime import duplication causing 500 error

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='2.1.12',
+    version='2.1.13',
 
     description='''Theme for internal CKAN build.''',
     long_description=long_description,
