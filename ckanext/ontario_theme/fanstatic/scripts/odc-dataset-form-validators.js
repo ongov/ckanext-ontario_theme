a44fc50711ee27e0cd744fa487270e0fb10554b6
AleexGreen <alex.green@ontario.ca>
2024-02-13 09:25:16 -0500

    [Task]: Limiting licence options when under review or restricted is selected (#508)
    * [Bug]: Site title should have a FR title field (#445)
    * Adds French input option for site title
    * automatic license selection with specific access level options

---
+++ ckanext/ontario_theme/fanstatic/scripts/odc-dataset-form-validators.js
@@ -0,0 +1,44 @@
+// Enable JavaScript's strict mode. Strict mode catches some common
+// programming errors and throws exceptions, prevents some unsafe actions from
+// being taken, and disables some confusing and bad JavaScript features.
+
+/*
+ * CKAN module function to limit the options of a conditional field 
+ * based on the user's selected choice on a triggered field
+ * 
+ * For example: When a user selects the access level of under review or restricted,
+ * the only option for users is "Not Applicable", while the other options of the Licence
+ * field are disabled
+ * 
+ * conditional_option
+ *  The option(s), in a comma separated string, that will trigger the function when selected
+ *  (E.g.: "under_review,restricted")
+ * trigger_field
+ *  The field that contains the conditional_option(s). (E.g.: Access Level)
+ * trigger_option
+ *  The option to be selected from the conditional field, disabling the other options. 
+ *  (E.g.: Not applicable licence)
+ */
+"use strict";
+
+ckan.module('conditional_field', function ($) {
+  return {
+    options : {
+      trigger_values: null,
+		},
+    initialize: function () {
+      this.trigger_values = (this.options.conditional_option).split(",");
+      $(this.options.trigger_field).on('change', jQuery.proxy(this._onChange, this));
+    },
+    _onChange: function (event) {
+      var option_selected = event.target.value
+      let id = $(this.el).attr('id');
+      if ($.inArray(option_selected, this.trigger_values) != -1) {
+        $(`#${id}`).val(this.options.trigger_option).change();
+        $(`#${id}`).find(':not(:selected)').prop('disabled', true);
+      } else {
+        $(`#${id}`).find(':not(:selected)').prop('disabled', false)
+      }
+    }
+  }
+});
