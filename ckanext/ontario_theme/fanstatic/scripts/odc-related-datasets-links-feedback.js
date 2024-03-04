414d77a9c6962977dabb72f7ec254c0aab2c9dd2
Kobikah Chandran <Kobikah.Chandran@ontario.ca>
2024-03-04 10:04:18 -0500

    Suggested changes and spacing corrections

--- ckanext/ontario_theme/fanstatic/scripts/odc-related-datasets-links-feedback.js
+++ ckanext/ontario_theme/fanstatic/scripts/odc-related-datasets-links-feedback.js
@@ -1,33 +1,19 @@
 //Shows the input field in the Related datasets section at the bottom and right-hand side of the page when user clicks on no as a radio button option
 
 (function () {
-  var noRadioButton = document.getElementById("radio-button-option-no");
-  var yesRadioButton = document.getElementById("radio-button-option-yes");
-  var noRadioButtonRight = document.getElementById("radio-button-option-no-right");
-  var yesRadioButtonRight = document.getElementById("radio-button-option-yes-right");
 
-  var explanationInput = document.getElementById("feedback-input-bottom");
-  var explanationInputRight = document.getElementById("feedback-input-right");
+  const yesNoRadioButtons = document.querySelectorAll('[id^="radio-button-option-"');
 
-  noRadioButton.addEventListener('change', handleRadioButton);
-  yesRadioButton.addEventListener('change', handleRadioButton);
-  noRadioButtonRight.addEventListener('change', handleRadioButtonRight);
-  yesRadioButtonRight.addEventListener('change', handleRadioButtonRight);
+  yesNoRadioButtons.forEach(element => {
+    element.addEventListener('change', handleRadioButton)
+  })
 
   function handleRadioButton() {
-    if (noRadioButton.checked) {
+    let explanationInput = document.getElementById(`feedback-input-${this.dataset.feedback}`)
+    if (this.value === 'option-no' && this.checked) {
       explanationInput.style.display = "block";
     } else {
       explanationInput.style.display = "none";
     }
   }
-
-  function handleRadioButtonRight() {
-    if (noRadioButtonRight.checked) {
-      explanationInputRight.style.display = "block";
-    } else {
-      explanationInputRight.style.display = "none";
-    }
-  }
-
 })();
