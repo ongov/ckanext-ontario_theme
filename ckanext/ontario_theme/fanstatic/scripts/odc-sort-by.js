fa8becd78d9dc930aa349d6c801487bae9b7b9b0
Alex Green <alex.green@ontario.ca>
2024-04-09 17:21:41 +0000

    close button

--- ckanext/ontario_theme/fanstatic/scripts/odc-sort-by.js
+++ ckanext/ontario_theme/fanstatic/scripts/odc-sort-by.js
@@ -1,13 +1,25 @@
 
 (function () {
     let sortByButton = document.querySelector(".show-sort-by");
-    // let hideSortBy = document.querySelector(".hide-sort-by");
+    const sortByModal = document.getElementById("sort-by-modal");
+    let closeSortButton = document.querySelector(".sort-by-close");
     sortByButton.addEventListener("click", function (e) {
         e.preventDefault();
         document.body.classList.add("sort-by-modal");
     })
-    /* hideSortBy.addEventListener("click", function (e) {
-        document.body.classList.remove("sort-by-modal");
-    })*/
+    window.onclick = function(e) {
+        if (e.target == sortByModal) {
+            closeModal();
+        }
+      }
+    closeSortButton.addEventListener("click", closeModal)
+    closeSortButton.addEventListener("keydown", (event) => {
+        if (event.key === " " || event.key === "Enter") {
+            closeModal();
+        }
+    })
 
+    function closeModal(event) {
+        document.body.classList.remove("sort-by-modal");
+    }
 })();
