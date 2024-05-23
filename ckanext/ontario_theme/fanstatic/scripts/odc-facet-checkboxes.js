d80966abdc71139fa9a2a89e4a831006c54c0ea5
Alex Green <alex.green@ontario.ca>
2024-05-23 20:35:10 +0000

    Facet option checkboxes, fully functional

---
+++ ckanext/ontario_theme/fanstatic/scripts/odc-facet-checkboxes.js
@@ -0,0 +1,16 @@
+// Enable JavaScript's strict mode. Strict mode catches some common
+// programming errors and throws exceptions, prevents some unsafe actions from
+// being taken, and disables some confusing and bad JavaScript features.
+
+"use strict";
+
+ckan.module('facet_checkboxes', function ($) {
+  return {
+    initialize: function () {
+          $(":checkbox").on('click', jQuery.proxy(this._onChange, this));
+    },
+      _onChange: function (event) {
+          window.location.href = event.target.value;
+    }
+  }
+});
