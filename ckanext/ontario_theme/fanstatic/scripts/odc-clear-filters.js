d0889a518a6c7eb73609bf6a8cef33dacbc13db0
Alex Green <alex.green@ontario.ca>
2024-06-04 15:58:33 +0000

    wrapping of pills and clear filters button

--- ckanext/ontario_theme/fanstatic/scripts/odc-clear-filters.js
+++ ckanext/ontario_theme/fanstatic/scripts/odc-clear-filters.js
@@ -3,7 +3,7 @@
     var facetsSelected = document.querySelectorAll(".filtered.pill");
 
     if (facetsSelected.length >= 2) {
-        clearFiltersButton.style.display = "block";
+        clearFiltersButton.style.display = "inline-block";
         clearFiltersButton.addEventListener('click', clearFilters);
     }
     function clearFilters() {
