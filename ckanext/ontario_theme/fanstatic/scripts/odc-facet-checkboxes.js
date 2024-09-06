4fe8be625cf4a305d97b8ab66a3614b70bbe0190
Alex Green <alex.green@ontario.ca>
2024-09-06 19:36:49 +0000

    HTMX and facets

--- ckanext/ontario_theme/fanstatic/scripts/odc-facet-checkboxes.js
+++ ckanext/ontario_theme/fanstatic/scripts/odc-facet-checkboxes.js
@@ -24,9 +24,11 @@ ckan.module('facet_checkboxes', function ($) {
         }).appendTo(hiddenFields);
       } else {
         $(`input[name='${event.target.name}'][value='${event.target.value}']`).remove();
+        //var form = $('form.search-form');
+        //form.submit();  
       }
-      let form = $('form.search-form');
-      form.submit();
+      let spinner = document.getElementById("spinner");
+      spinner.setAttribute('aria-hidden', 'false');
     }
   }
 });
