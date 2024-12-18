d6dbd5bff0719ca027c231a1020c274d50b495be
katiRG <cnangini@gmail.com>
2024-12-18 17:04:46 -0500

    try new check for existing WEB resources

--- ckanext/ontario_theme/resource.py
+++ ckanext/ontario_theme/resource.py
@@ -222,28 +222,40 @@ class EditView(MethodView):
         ))
 
         print('HEJ resourcepy def post of EditView')
+        resource = get_action('resource_show')(None, {"id": resource_id})
+        print('HEJ resourcepy junk: ', resource)
 
         # we don't want to include save as it is part of the form
         del data[u'save']
 
         data[u'package_id'] = id
+        # try:
+        #     if resource_id:
+        #         try:
+        #             info=get_action('datastore_search')(
+        #                                 data_dict={'id': resource_id})
+        #             print('HEJ resourcepy info: ', info)
+        #             data[u'id'] = resource_id
+        #             get_action(u'resource_update')(context, data)
+        #         # except:
+        #         #     print('HEJ resourcepy datastore search failed for ', resource_id)
+        #         #     get_action(u'resource_create')(context, data)
+        #         except Exception as e:
+        #             print('HEJ resourcepy datastore search failed for ', resource_id)
+        #             log.error(e)
+        #             if 'validation_status' not in resource:
+        #                 print('HEJ resourcepy validation_status not in resource, create new resource ')
+        #                 get_action(u'resource_create')(context, data)
+                    
+        #     else:
+        #         get_action(u'resource_create')(context, data)
         try:
             if resource_id:
-                try:
-                    info=get_action('datastore_search')(
-                                        data_dict={'id': resource_id})
-                    print('HEJ resourcepy info: ', info)
-                    data[u'id'] = resource_id
+                data[u'id'] = resource_id
+                if 'validation_status' in resource:
                     get_action(u'resource_update')(context, data)
-                # except:
-                #     print('HEJ resourcepy datastore search failed for ', resource_id)
-                #     get_action(u'resource_create')(context, data)
-                except Exception as e:
-                    print('HEJ resourcepy datastore search failed for ', resource_id)
-                    get_action(u'resource_create')(context, data)
-                    log.error(e)
-            else:
-                get_action(u'resource_create')(context, data)
+              
+
         except ValidationError as e:
             errors = e.error_dict
             error_summary = e.error_summary
