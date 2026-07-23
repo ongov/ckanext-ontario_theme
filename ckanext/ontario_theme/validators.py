9404adfa3de15b96fdec6c2658a1a00032351997
Abinash Jegatheeswaran <abinashjegan17@gmail.com>
2026-07-23 15:21:24 -0400

    DATA-1519: Fixed navigation issue after adding URL/File resource

--- ckanext/ontario_theme/validators.py
+++ ckanext/ontario_theme/validators.py
@@ -261,3 +261,21 @@ def public_https_url_validator(value, context):
     ))
 
     return normalized
+
+def public_https_resource_url_validator(key, data, errors, context):
+
+    url_type_key = key[:-1] + ('url_type',)
+
+    if data.get(url_type_key) == 'upload':
+        return
+
+    value = data.get(key)
+
+    # Leave missing values for scheming_required to handle.
+    if not value:
+        return
+
+    try:
+        data[key] = public_https_url_validator(value, context)
+    except Invalid as error:
+        errors.setdefault(key, []).append(str(error))
